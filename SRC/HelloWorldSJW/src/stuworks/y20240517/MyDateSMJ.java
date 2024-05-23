febe3bdba714b661a6adcaa7ac366366ea22d5d2
Minji Seo <someg0124@gmail.com>
2024-05-23 13:17:28 +0900

    서민지 311p 연습문제 5~6

--- SRC/HelloWorldSJW/src/stuworks/y20240517/MyDateSMJ.java
+++ SRC/HelloWorldSJW/src/stuworks/y20240517/MyDateSMJ.java
@@ -2,9 +2,8 @@ package stuworks.y20240517;
 
 public class MyDateSMJ {
 	
-	private int day;
-	private int month;
-	private int year;
+	private int day, month, year;
+	private final int MONTH_OF_DAYS[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
 	public int getDay() {
 		return day;
@@ -25,6 +24,9 @@ public class MyDateSMJ {
 		this.year = year;
 	}
 	
+	public MyDateSMJ() {
+	}
+	
 	public MyDateSMJ(int day, int month, int year) {
 		this.day = day;
 		this.month = month;
@@ -32,30 +34,20 @@ public class MyDateSMJ {
 	}
 	
 	public boolean isValid() {
-		
-		if (month < 1 || month > 12) {
-			return false;
-		}
-		
-		if (month == 2) {
-			if (day < 1 || day > 28) {
-				return false;
+		// 월 체크
+		if (month >= 1 && month <= 12) {
+			// 일 체크 (시작은 무조건 1보다 커야 된다. 끝날짜는 배열에 해당 월의 날짜보다 작아야 한다.)
+			if (day >= 1 && day <= MONTH_OF_DAYS[month - 1]) {
+				return true;
 			}
 		}
-		
-			return true;
-		}
+		return false;
+	}
 
-	
 	public static void main(String[] args) {
-
-		MyDateSMJ date1 = new MyDateSMJ(30, 2, 2000);
+		MyDateSMJ date1 = new MyDateSMJ(31, 8, 2000);
 		System.out.println(date1.isValid());
 		MyDateSMJ date2 = new MyDateSMJ(2, 10, 2006);
 		System.out.println(date2.isValid());
-		
 	}
-	
-	
-	
 } // end class
