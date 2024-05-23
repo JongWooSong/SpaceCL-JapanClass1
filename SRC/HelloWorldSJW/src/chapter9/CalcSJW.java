7fed611938076b88ad15a27e57bd0db1a77a584a
송종우 <itbong@aniss.kr>
2024-05-23 11:50:25 +0900

    추상 클래스 수정

--- SRC/HelloWorldSJW/src/chapter9/CalcSJW.java
+++ SRC/HelloWorldSJW/src/chapter9/CalcSJW.java
@@ -2,6 +2,11 @@ package chapter9;
 
 public abstract class CalcSJW {
 
+	public static final int T_PLUS = 0;
+	public static final int T_MINUS = 1;
+	public static final int T_MULTIPLE = 2;
+	public static final int T_DEVIDE = 3;
+	
 	public abstract double plus(int a, int b);
 	
 	public abstract double minus(int a, int b);
@@ -10,6 +15,6 @@ public abstract class CalcSJW {
 	
 	public abstract double devide(int a, int b);
 	
-	public abstract void showResult(double result);
+	public abstract void showResult(int type, int a, int b);
 	
 }
