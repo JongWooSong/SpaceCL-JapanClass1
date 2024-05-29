cbdb474c1386dfff58f5193ae4f9ecaf6858278c
송종우 <itbong@aniss.kr>
2024-05-29 10:46:19 +0900

    설계수정

--- SRC/HelloWorldSJW/src/chapter12/exam/SchoolLibrary.java
+++ SRC/HelloWorldSJW/src/chapter12/exam/SchoolLibrary.java
@@ -8,8 +8,11 @@ public abstract class SchoolLibrary {
 	/** 책 타이틀 목록 **/
 	public abstract String getBookTitleList();
 	
-	/** 책 찾기 by title or author **/
-	public abstract Book getBook(String bookTitle, String author);
+	/** 책 찾기 by title **/
+	public abstract Book getBookByTitle(String bookTitle);
+	
+	/** 책 찾기 by author **/
+	public abstract Book getBookByAuthor(String author);
 	
 	/** 도서관 보유 전체 책 수 **/
 	public abstract int getBookNum();
