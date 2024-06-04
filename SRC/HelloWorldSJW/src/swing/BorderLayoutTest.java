f6ca0e22eefc39d9b5434b95a558bc06790f7173
송종우 <itbong@aniss.kr>
2024-06-04 11:06:44 +0900

    스윙

--- SRC/HelloWorldSJW/src/swing/BorderLayoutTest.java
+++ SRC/HelloWorldSJW/src/swing/BorderLayoutTest.java
@@ -5,16 +5,49 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Panel;
-import java.awt.TextField;import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 
 public class BorderLayoutTest extends JFrame {
 
+	/**
+	 * 로그인 성공여부를 체크한다.
+	 * @param tfId
+	 * @param tfPw
+	 * @return true:로그인 성공, false:로그인 실패
+	 */
+	public boolean isLoginCheck(JTextField tfId, JPasswordField tfPw) {
+		if( "".equals(tfId.getText())  ) {
+			JOptionPane.showMessageDialog(null, "ID를 입력하세요");
+			tfId.requestFocus();
+			return false;
+		}
+		else if( "".equals(new String(tfPw.getPassword()))  ) {
+			JOptionPane.showMessageDialog(null, "PW를 입력하세요");
+			tfPw.requestFocus();
+			return false;
+		}
+		else if( "abcd".equals(tfId.getText()) && "0987!".equals(tfPw.getText()) ) {
+			JOptionPane.showMessageDialog(null, "로그인 성공!!");
+			return true;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "로그인 실패!!");
+			tfPw.requestFocus();
+			return false;
+		}
+	}
+	
+	
 	
 	public static void main(String[] args) {
 			
@@ -42,7 +75,7 @@ public class BorderLayoutTest extends JFrame {
 		Label lb2 = new Label("PW: ");
 		lb2.setAlignment(Label.CENTER);
 		
-		TextField tfId = new TextField();
+		JTextField tfId = new JTextField();
 		JPasswordField tfPw = new JPasswordField(3);
 
 		p1.add(lb1);
@@ -55,20 +88,28 @@ public class BorderLayoutTest extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				if( "".equals(tfId.getText())  ) {
-					JOptionPane.showMessageDialog(null, "ID를 입력하세요");
-					tfId.requestFocus();
-				}
-				else if( "".equals(new String(tfPw.getPassword()))  ) {
-					JOptionPane.showMessageDialog(null, "PW를 입력하세요");
-					tfPw.requestFocus();
-				}
-				else if( "abcd".equals(tfId.getText()) && "0987!".equals(tfPw.getText()) ) {
-					JOptionPane.showMessageDialog(null, "로그인 성공!!");
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "로그인 실패!!");
-					tfPw.requestFocus();
+				bl.isLoginCheck(tfId, tfPw);
+				
+			}
+		});
+		
+		//키보드 엔터시
+		tfPw.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				
+				if( e.getKeyChar() == KeyEvent.VK_ENTER ) {
+					System.out.println("엔터");
+					bl.isLoginCheck(tfId, tfPw);
 				}
 				
 			}
@@ -77,6 +118,17 @@ public class BorderLayoutTest extends JFrame {
 		
 		
 		bl.setVisible(true);
-	}
+	}//end main()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
