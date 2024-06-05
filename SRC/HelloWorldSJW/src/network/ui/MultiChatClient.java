a3feb7ef3ca080de5936863a4ba59b116004e233
송종우 <itbong@aniss.kr>
2024-06-05 11:57:10 +0900

    ㅏㅏ

--- SRC/HelloWorldSJW/src/network/ui/MultiChatClient.java
+++ SRC/HelloWorldSJW/src/network/ui/MultiChatClient.java
@@ -1,5 +1,7 @@
 package network.ui;
 
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class MultiChatClient {
@@ -13,13 +15,25 @@ public class MultiChatClient {
 		try {
 			mSocket = new Socket( mChatView.mIp, 9999 );
 			String msg = "서버에 접속 되었다." + mSocket.isConnected();
-			mChatView.mJta.append(msg);
-			
+			mChatView.mJta.append(msg + "\n");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
+
+	//서버로 메시지 전송
+	public void sendMsg(String msg) {
+		try {
+			OutputStream os = mSocket.getOutputStream();
+			BufferedOutputStream bos = new BufferedOutputStream(os);
+			byte[] bytes = msg.getBytes();
+			bos.write(bytes);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}//end method
+	
 	
 	
 	
