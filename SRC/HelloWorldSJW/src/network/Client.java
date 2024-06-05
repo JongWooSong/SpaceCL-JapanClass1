3cc4a22be3ba26136b43eb1872fb7fd041e02d9f
송종우 <itbong@aniss.kr>
2024-06-05 12:15:10 +0900

    ㅎㄹㄽㅎ

--- SRC/HelloWorldSJW/src/network/Client.java
+++ SRC/HelloWorldSJW/src/network/Client.java
@@ -14,14 +14,14 @@ public class Client {
 		
 		//포트번호 2012 번으로 서버에 연결 시도
 		try {
-			Socket sock = new Socket("127.0.0.1", 3285);
+			Socket sock = new Socket("127.0.0.1", 9999);
 			System.out.println( new Date() + " ==> " + "2012 포트로 서버 접속에 성공 하였습니다.");
 			
-			Thread.sleep(1000);
-			System.out.println( new Date() + " ==> " + "클라이언트가 서버로 메시지를 송신 합니다.");
+			//System.out.println( new Date() + " ==> " + "클라이언트가 서버로 메시지를 송신 합니다.");
 			BufferedOutputStream bw = new BufferedOutputStream( sock.getOutputStream() );
 			bw.write( msg.getBytes() );
 			bw.flush();//즉시전송 
+			
 			
 			//클라이언트가 서버의 메시지를 받는다.
 //			BufferedReader br = new BufferedReader( new InputStreamReader( sock.getInputStream() ) );
--- SRC/HelloWorldSJW/src/network/Server.java
+++ SRC/HelloWorldSJW/src/network/Server.java
@@ -1,9 +1,9 @@
 package network;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
@@ -13,32 +13,34 @@ public class Server {
 	public static void main(String[] args) {
 		
 		try {
-			ServerSocket ss = new ServerSocket(3285); //포트번호
+		
+			ServerSocket ss = new ServerSocket(9999); //포트번호
 			System.out.println( new Date() + " ==> " + "서버: 클라이언트의 접속을 기다립니다.");
 			
-			//클라이언트가 접속을 하면 Socket 객체로써 반환된다.
-			Socket soc = ss.accept();
-			
-			//접속한 클라이언트의 정보 출력
-			String msg = "서버: " + soc.getInetAddress() + " 클라이언트와 ";
-			msg += soc.getLocalPort() + " 포트로 연결 되었습니다.";
-			System.out.println( new Date() + " ==> " + msg );
-			
-			//클라이언트의 메세지를 받기위해 스트림을 가져온다.
-			BufferedReader br = new BufferedReader( new InputStreamReader( soc.getInputStream() ) );
-			//클라이언트로부터 한줄을 읽어온다.
-			String clientMsg = br.readLine();
-			System.out.println( new Date() + " ==> " + "서버(수신): " + clientMsg);
-			
-			//서버가 클라이언트로 메시지를 보낸다.
-			PrintWriter pw = new PrintWriter( new OutputStreamWriter( soc.getOutputStream() ) );
-			pw.write( new Date() + " ==> " + "안녕 클라이언트야 접속을 환영해!" );
+			while(true) {
+				//클라이언트가 접속을 하면 Socket 객체로써 반환된다.
+				Socket soc = ss.accept();
+				
+				//접속한 클라이언트의 정보 출력
+				String msg = "서버: " + soc.getInetAddress() + " 클라이언트와 ";
+				msg += soc.getLocalPort() + " 포트로 연결 되었습니다.";
+				System.out.println( new Date() + " ==> " + msg );
+				
+				//클라이언트의 메세지를 받기위해 스트림을 가져온다.
+				BufferedReader br = new BufferedReader( new InputStreamReader( soc.getInputStream() ) );
+				//클라이언트로부터 한줄을 읽어온다.
+				String clientMsg = br.readLine();
+				System.out.println( new Date() + " ==> " + "서버(수신): " + clientMsg);
+			}
+//			//서버가 클라이언트로 메시지를 보낸다.
+//			BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( soc.getOutputStream() ) );
+//			bw.write( new Date() + " ==> " + "안녕 클라이언트야 접속을 환영해!" );
 			
 			//사용했던 스트림 및 소켓을 닫아준다. 안닫아주면 메모리 잡아묵고 좀비 프로그램이 됨.
-			pw.close();
-			br.close();
-			soc.close();
-			ss.close();
+//			bw.close();
+			//br.close();
+			//soc.close();
+			//ss.close();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
