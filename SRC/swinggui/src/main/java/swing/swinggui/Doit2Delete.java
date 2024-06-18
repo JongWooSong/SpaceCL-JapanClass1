0760997a931c58fac4fe82c000c04ed56d5d0849
송종우 <itbong@aniss.kr>
2024-06-18 09:43:25 +0900

    소스

---
+++ SRC/swinggui/src/main/java/swing/swinggui/Doit2Delete.java
@@ -0,0 +1,53 @@
+package swing.swinggui;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Doit2Delete {
+
+	public static void main(String[] args) {
+		Doit2Delete doit2Update = new Doit2Delete();
+		
+		DoitBean doitBean = new DoitBean();
+		doitBean.setCol1("102");
+		
+		doit2Update.update(doitBean);
+	}
+	
+	public void update(DoitBean updBean) {
+		Connection conn = null;
+		//insert, update, delete
+		PreparedStatement pstmt = null;
+		
+		try { 
+			//1.드라이버 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver"); //JDBC Mysql Driver (pom.xml)
+			
+			//2.연결하기
+			String url = "jdbc:mysql://localhost/studydb1";
+			conn = DriverManager.getConnection(url, "spacecl", "1234"); //id, pw로 디비연결
+			System.out.println("디비 연결 성공!!!");
+			
+			//3.쿼리준비
+			String sql = "DELETE FROM doit2 WHERE col1=?";
+			pstmt = conn.prepareStatement(sql);
+			
+			//4.데이터 binding
+			pstmt.setString(1, updBean.getCol1() );
+			
+			//5.쿼리실행 
+			int cnt = pstmt.executeUpdate();
+			//insert 된 row 수
+			if(cnt == 0) {
+				System.out.println("데이터 삭제 실패");
+			} else {
+				System.out.println("데이터 삭제 성공");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
