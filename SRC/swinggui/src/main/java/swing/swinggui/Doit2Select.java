170d0d43513916c36169080e94c0212ec1b2e17f
송종우 <itbong@aniss.kr>
2024-06-18 09:42:13 +0900

    수업소스 커밋

--- SRC/swinggui/src/main/java/swing/swinggui/Doit2Select.java
+++ SRC/swinggui/src/main/java/swing/swinggui/Doit2Select.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Doit2Jdbc {
+public class Doit2Select {
 
 	public static void main(String[] args) {
 
