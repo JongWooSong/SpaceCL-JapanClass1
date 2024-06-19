a92660500fa51ab53fe798956764b4373aa7fd7c
송종우 <itbong@aniss.kr>
2024-06-19 12:07:11 +0900

    mk

--- SRC/swingguiSJW/src/main/java/board/db/MemberCRUD.java
+++ SRC/swingguiSJW/src/main/java/board/db/MemberCRUD.java
@@ -1,6 +1,7 @@
 package board.db;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class MemberCRUD extends CommonCRUD {
 
@@ -11,10 +12,29 @@ public class MemberCRUD extends CommonCRUD {
 	 * @param mBean
 	 * @return
 	 */
-	public int insertMember(Connection conn, MemberBean mBean) {
-	
+	public int insertMember(MemberBean mBean) {
 		
+		Connection conn = getConnection();
+		int cnt = 0;
 		
+		try {
+			//3.쿼리준비
+			String sql = "INSERT INTO member(id, pw, name, email) VALUES(?, ?, ?, ?)";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			
+			//4.데이터 binding
+			pstmt.setString(1, mBean.getId() );
+			pstmt.setString(2, mBean.getPw());
+			pstmt.setString(3, mBean.getName());
+			pstmt.setString(4, mBean.getEmail());
+			
+			//5.쿼리실행 
+			cnt = pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return cnt;
 	}
 	
 }
