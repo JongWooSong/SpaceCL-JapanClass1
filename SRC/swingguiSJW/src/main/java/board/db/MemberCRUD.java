0e47d97f735e7614ea5809670f732c5aa6f75d38
송종우 <itbong@aniss.kr>
2024-06-19 11:33:35 +0900

    s

--- SRC/swingguiSJW/src/main/java/board/db/MemberCRUD.java
+++ SRC/swingguiSJW/src/main/java/board/db/MemberCRUD.java
@@ -1,5 +1,20 @@
 package board.db;
 
-public class MemberCRUD {
+import java.sql.Connection;
 
+public class MemberCRUD extends CommonCRUD {
+
+	
+	/**
+	 * 회원을 추가한다.
+	 * @param conn
+	 * @param mBean
+	 * @return
+	 */
+	public int insertMember(Connection conn, MemberBean mBean) {
+	
+		
+		
+	}
+	
 }
