31e20c671c372811513ee7af30b63ba1ea7b0d5c
송종우 <itbong@aniss.kr>
2024-07-05 11:50:17 +0900

    r

--- SRC/HelloSpringSJW/src/main/java/com/hello/hellospring/common/daos/MemberDao.java
+++ SRC/HelloSpringSJW/src/main/java/com/hello/hellospring/common/daos/MemberDao.java
@@ -1,5 +1,13 @@
 package com.hello.hellospring.common.daos;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.hello.hellospring.common.bean.MemberBean;
+
+@Mapper
 public interface MemberDao {
 
+	//1건 조회
+	public MemberBean selectMember(MemberBean bean);
+	
 }
