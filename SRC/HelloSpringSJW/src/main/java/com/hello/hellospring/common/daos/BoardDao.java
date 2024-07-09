953b81cc6be16975c49422e437c68201acf41077
양진우 <didgnldkrl@gmail.com>
2024-07-09 10:36:09 +0900

    ??

--- SRC/HelloSpringSJW/src/main/java/com/hello/hellospring/common/daos/BoardDao.java
+++ SRC/HelloSpringSJW/src/main/java/com/hello/hellospring/common/daos/BoardDao.java
@@ -1,5 +1,7 @@
 package com.hello.hellospring.common.daos;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.hello.hellospring.common.bean.BoardBean;
@@ -8,5 +10,20 @@ import com.hello.hellospring.common.bean.BoardBean;
 public interface BoardDao {
 
 	public int deleteBoardFromMemberNo(BoardBean boardBean);
+	
+	//1건 조회
+	public BoardBean selectBoard(BoardBean bean);
+	
+	//여러건 조회
+	public List<BoardBean> selectBoardList(BoardBean bean);
+	
+	//1건 입력
+	public int insertBoard(BoardBean bean);
+	
+	//1건 수정
+	public int updateBoard(BoardBean bean);
+	
+	//1건 삭제
+	public int deleteBoard(BoardBean bean);
 	
 }
--- SRC/HelloSpringSJW/src/main/java/com/hello/hellospring/service/BoardService.java
+++ SRC/HelloSpringSJW/src/main/java/com/hello/hellospring/service/BoardService.java
@@ -1,10 +1,38 @@
 package com.hello.hellospring.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.hello.hellospring.common.bean.BoardBean;
+import com.hello.hellospring.common.daos.BoardDao;
 
 @Service
 public class BoardService {
 	
+	@Autowired
+	private BoardDao boardDao;
 	
 	
+	public int insertBoard (BoardBean bean) {
+		return boardDao.insertBoard(bean);
+	}
+	
+	public int updateBoard (BoardBean bean) {
+		return boardDao.updateBoard(bean);
+	}
+	
+	public int deleteBoard (BoardBean bean) {
+		return boardDao.deleteBoard(bean);
+	}
+
+	public BoardBean selectBoard(BoardBean bean) {
+		return boardDao.selectBoard(bean);
+	}
+
+	public List<BoardBean> selecetBoardList(BoardBean bean) {
+		return boardDao.selectBoardList(bean);
+	}
+
 }
